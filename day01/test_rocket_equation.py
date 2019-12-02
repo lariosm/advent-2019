7d8f573e46d64cf9f00a06ef40ca12f30d5c3c39
Manuel Larios <mlarios12@wou.edu>
2019-12-01 23:21:40 -0800

    Working that module of mass 100756 returns 33583

--- day01/test_rocket_equation.py
+++ day01/test_rocket_equation.py
@@ -11,3 +11,7 @@ def test_mass_fourteen_returns_two():
 
 def test_mass_1969_returns_654():
     assert fuel_calculator(1969) == 654
+
+
+def test_mass_100756_returns_33583():
+    assert fuel_calculator(100756) == 33583
