0d1cd82f5e12fa363587ee9321371f21b3fbe6bb
Manuel Larios <mlarios12@wou.edu>
2019-12-01 23:19:04 -0800

    Working that module of mass 1969 returns 654

--- day01/test_rocket_equation.py
+++ day01/test_rocket_equation.py
@@ -3,3 +3,11 @@ from rocket_equation import fuel_calculator
 
 def test_mass_twelve_returns_two():
     assert fuel_calculator(12) == 2
+
+
+def test_mass_fourteen_returns_two():
+    assert fuel_calculator(14) == 2
+
+
+def test_mass_1969_returns_654():
+    assert fuel_calculator(1969) == 654
