fa4b34a7b1b129b399e49d025876fa76f300c336
Manuel Larios <mlarios12@wou.edu>
2019-12-02 19:04:50 -0800

    Working module with fuel cost returns 2

--- day01/rocket_equation.py
+++ day01/rocket_equation.py
@@ -2,20 +2,13 @@ import math
 
 
 def fuel_calculator(module_mass):
-    """
-    returns amount of fuel needed for a given module.
-    Parameters:
-    module_mass (int): mass of module
-
-    Returns:
-    int: Amount of fuel needed for the module
-    """
+    """returns amount of fuel needed for a given module"""
     return math.floor(module_mass / 3) - 2
 
 
 def fuel_requirement_sum():
     """returns sum amount of fuel needed for all modules"""
-    fuel_counter = 0  # Tracks total sum of fuel needed for all modules
+    fuel_counter = 0  # Tracks total sum of fuel needed
 
     with open('input.txt') as file:
         for value in file:
@@ -23,5 +16,15 @@ def fuel_requirement_sum():
     return fuel_counter
 
 
+def module_with_fuel_cost(fuel_mass, fuel_sum=0):
+    """returns amount of fuel needed for a given module with added fuel mass"""
+    if fuel_mass <= 0:
+        return fuel_sum
+    else:
+        required_fuel = fuel_calculator(fuel_mass)
+        return module_with_fuel_cost(required_fuel, fuel_sum +
+                                     max(0, required_fuel))
+
+
 if __name__ == '__main__':
-    print(fuel_requirement_sum())
+    print(fuel_requirement_sum())  # Part 1
--- day01/test_rocket_equation.py
+++ day01/test_rocket_equation.py
@@ -1,4 +1,5 @@
-from rocket_equation import fuel_calculator, fuel_requirement_sum
+from rocket_equation import fuel_calculator, fuel_requirement_sum, \
+    module_with_fuel_cost
 
 
 def test_mass_twelve_returns_two():
@@ -19,3 +20,7 @@ def test_mass_100756_returns_33583():
 
 def test_fuel_requirements_contains_int():
     assert isinstance(fuel_requirement_sum(), int)
+
+
+def test_mass_14_with_fuel_mass_returns_2():
+    assert module_with_fuel_cost(14) == 2
