9ce57dc7d0053484f8a5883414ca80e81022b75c
Manuel Larios <mlarios12@wou.edu>
2019-12-03 23:31:12 -0800

    Working list operation

--- day02/program_alarm.py
+++ day02/program_alarm.py
@@ -2,3 +2,30 @@ def intcode_as_array():
     with open('input.txt') as file:
         int_list = (list(map(int, file.read().split(","))))
     return int_list
+
+
+def perform_list_operation():
+    index_counter = 0
+    intcode_list = intcode_as_array()
+
+    # Preliminary data swap as requested by puzzle
+    intcode_list[1] = 12
+    intcode_list[2] = 2
+
+    # Looping through list, performing operations
+    while False:
+        opcode = intcode_list[index_counter]
+        input_one = intcode_list[index_counter + 1]
+        input_two = intcode_list[index_counter + 2]
+
+        if opcode == 1:
+            intcode_list[index_counter + 3] = input_one + input_two
+        elif opcode == 2:
+            intcode_list[index_counter + 3] = input_one + input_two
+        elif opcode == 99:
+            break
+        else:
+            return "This program has encountered an error"
+        index_counter += 4  # Steps forward 4 positions
+
+    return intcode_list[0]
--- day02/test_program_alarm.py
+++ day02/test_program_alarm.py
@@ -1,4 +1,4 @@
-from program_alarm import intcode_as_array
+from program_alarm import intcode_as_array, perform_list_operation
 
 
 def test_array_has_ten_elements():
@@ -9,3 +9,12 @@ def test_array_has_ten_elements():
 def test_one_exists_at_position_four():
     intcode_list = intcode_as_array()
     assert intcode_list[4] == 1
+
+
+def test_three_exists_at_position_seven():
+    intcode_list = intcode_as_array()
+    assert intcode_list[7] == 3
+
+
+def test_list_operation_returns_int():
+    assert isinstance(perform_list_operation(), int)
