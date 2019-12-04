75478aea44c4f850597bbd5e5368ea7a758cc128
Manuel Larios <mlarios12@wou.edu>
2019-12-03 19:43:46 -0800

    Tested that 'intcode' values are in their correct positions

--- day02/test_program_alarm.py
+++ day02/test_program_alarm.py
@@ -4,3 +4,8 @@ from program_alarm import intcode_as_array
 def test_array_has_ten_elements():
     intcode_list = intcode_as_array()
     assert len(intcode_list) > 9
+
+
+def test_one_exists_at_position_four():
+    intcode_list = intcode_as_array()
+    assert intcode_list[4] == 1
