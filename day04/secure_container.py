b86e75164141e7095a0ced6f4b28f07de8c2869e
Manuel Larios <mlarios12@wou.edu>
2019-12-05 00:21:50 -0800

    Tested that digit adjacency function works

--- day04/secure_container.py
+++ day04/secure_container.py
@@ -2,12 +2,20 @@ def is_six_digits(number):
     return len(str(number)) == 6
 
 
+def contains_adjacent_digits(number):
+    digits = list(str(number))
+    for i in range(len(digits) - 1):
+        if(digits[i] == digits[i + 1]):
+            return True
+    return False
+
+
 def password_criteria():
     puzzle_input = list(map(int, "193651-649729".split("-")))
     for i in range(puzzle_input[0], puzzle_input[1] + 1, 1):
-        if i == 193651:
+        if(i == 193651):
             print("193651 was read")
-        if i == 649729:
+        if(i == 649729):
             print("649729 was read")
 
 
--- day04/test_secure_container.py
+++ day04/test_secure_container.py
@@ -1,6 +1,11 @@
-from secure_container import is_six_digits
+from secure_container import is_six_digits, contains_adjacent_digits
 
 
 def test_six_digit_function_works():
     assert is_six_digits(251903)
     assert not is_six_digits(3256)
+
+
+def test_contains_adjacent_works():
+    assert contains_adjacent_digits(122545)
+    assert not contains_adjacent_digits(412390)
