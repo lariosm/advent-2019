a11a018e257acc419a42cb95c6362643f43823ba
Manuel Larios <mlarios12@wou.edu>
2019-12-04 20:45:31 -0800

    Working solution. A function needs cleanup later on

--- day02/program_alarm.py
+++ day02/program_alarm.py
@@ -5,7 +5,6 @@ def intcode_as_array():
 
 
 def perform_list_operation():
-    index_counter = 0
     intcode_list = intcode_as_array()
 
     # Preliminary data swap as requested by puzzle
@@ -13,11 +12,13 @@ def perform_list_operation():
     intcode_list[2] = 2
 
     # Looping through list, performing operations
-    while True:
-        opcode = intcode_list[index_counter]
-        input_one = intcode_list[index_counter + 1]
-        input_two = intcode_list[index_counter + 2]
-        output_index = intcode_list[index_counter + 3]
+    for index in range(0, len(intcode_list) - 1, 4):
+        opcode = intcode_list[index]
+        if opcode == 99:
+            break
+        input_one = intcode_list[index + 1]
+        input_two = intcode_list[index + 2]
+        output_index = intcode_list[index + 3]
 
         if opcode == 1:
             intcode_list[output_index] = (intcode_list[input_one] +
@@ -25,14 +26,48 @@ def perform_list_operation():
         elif opcode == 2:
             intcode_list[output_index] = (intcode_list[input_one] *
                                           intcode_list[input_two])
-        elif opcode == 99:
-            break
         else:
             return "This program has encountered an error"
-        index_counter += 4  # Steps forward 4 positions
+
+    return intcode_list[0]
+
+
+# REMINDER: Function below needs major cleanup.
+def search_noun_verb():
+    for noun in range(100):
+        for verb in range(100):
+            intcode_list = intcode_as_array()
+
+            # Preliminary data swap as requested by puzzle
+            intcode_list[1] = noun
+            intcode_list[2] = verb
+
+            # Looping through list, performing operations
+            for index in range(0, len(intcode_list) - 1, 4):
+                opcode = intcode_list[index]
+                if opcode == 99:
+                    break
+                input_one = intcode_list[index + 1]
+                input_two = intcode_list[index + 2]
+                output_index = intcode_list[index + 3]
+
+                if opcode == 1:
+                    intcode_list[output_index] = (intcode_list[input_one] +
+                                                  intcode_list[input_two])
+                elif opcode == 2:
+                    intcode_list[output_index] = (intcode_list[input_one] *
+                                                  intcode_list[input_two])
+                else:
+                    return "This program has encountered an error"
+
+                if intcode_list[0] == 19690720:
+                    print("We've found 19690720!")
+                    print("100 * " + str(noun) + " + " + str(verb) + " = " +
+                          str(100 * noun + verb))
 
     return intcode_list[0]
 
 
 if __name__ == '__main__':
-    print(perform_list_operation())
+    print(perform_list_operation())  # part 1
+    print(search_noun_verb())  # part 2
--- day02/test_program_alarm.py
+++ day02/test_program_alarm.py
@@ -1,4 +1,5 @@
-from program_alarm import intcode_as_array, perform_list_operation
+from program_alarm import intcode_as_array, perform_list_operation,\
+    search_noun_verb
 
 
 def test_array_has_ten_elements():
@@ -22,3 +23,7 @@ def test_list_operation_returns_int():
 
 def test_index_zero_is_not_one():
     assert perform_list_operation() != 1
+
+
+def test_search_operation_returns_int():
+    assert isinstance(search_noun_verb(), int)
