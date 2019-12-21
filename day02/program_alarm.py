1dbad18ebaaab9a09280e3f250e5252e296721f0
Manuel Larios <mlarios12@wou.edu>
2019-12-21 14:36:42 -0800

    Removed duplicate code from Day 2 puzzle

--- day02/program_alarm.py
+++ day02/program_alarm.py
@@ -4,12 +4,12 @@ def intcode_as_array():
     return int_list
 
 
-def perform_list_operation():
+def perform_list_operation(noun=12, verb=2):
     intcode_list = intcode_as_array()
 
     # Preliminary data swap as requested by puzzle
-    intcode_list[1] = 12
-    intcode_list[2] = 2
+    intcode_list[1] = noun
+    intcode_list[2] = verb
 
     # Looping through list, performing operations
     for index in range(0, len(intcode_list) - 1, 4):
@@ -26,46 +26,17 @@ def perform_list_operation():
         elif opcode == 2:
             intcode_list[output_index] = (intcode_list[input_one] *
                                           intcode_list[input_two])
-        else:
+        else:  # Any other opcode read results in an error
             return "This program has encountered an error"
 
     return intcode_list[0]
 
 
-# REMINDER: Function below needs major cleanup.
 def search_noun_verb():
     for noun in range(100):
         for verb in range(100):
-            intcode_list = intcode_as_array()
-
-            # Preliminary data swap as requested by puzzle
-            intcode_list[1] = noun
-            intcode_list[2] = verb
-
-            # Looping through list, performing operations
-            for index in range(0, len(intcode_list) - 1, 4):
-                opcode = intcode_list[index]
-                if opcode == 99:
-                    break
-                input_one = intcode_list[index + 1]
-                input_two = intcode_list[index + 2]
-                output_index = intcode_list[index + 3]
-
-                if opcode == 1:
-                    intcode_list[output_index] = (intcode_list[input_one] +
-                                                  intcode_list[input_two])
-                elif opcode == 2:
-                    intcode_list[output_index] = (intcode_list[input_one] *
-                                                  intcode_list[input_two])
-                else:
-                    return "This program has encountered an error"
-
-                if intcode_list[0] == 19690720:
-                    print("We've found 19690720!")
-                    print("100 * " + str(noun) + " + " + str(verb) + " = " +
-                          str(100 * noun + verb))
-
-    return intcode_list[0]
+            if(perform_list_operation(noun, verb) == 19690720):
+                return 100 * noun + verb
 
 
 if __name__ == '__main__':
